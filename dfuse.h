569533e238f9dbc6bd7e8ef6f67a2891b34d7b8d
arp <anrope@gmail.com>
2010-06-28 15:12:18 -0400

    produces good dfuse files from bins, other than incorrect crc issue

--- dfuse.h
+++ dfuse.h
@@ -1,40 +1,48 @@
 //DfuSe file structure referenced from STM document UM0391
 //see UM0391 for descriptions of all fields
 
+#define uint8_t u_int8_t
+#define uint16_t u_int16_t
+#define uint32_t u_int32_t
+
+#define PREFIXLEN 11
+#define SUFFIXLEN 16
+#define TARPREFIXLEN 274
+
 typedef struct {
 	char signature[5];
-	int8_t version;
-	int32_t dfu_image_size;
-	int8_t targets;
+	uint8_t version;
+	uint32_t dfu_image_size;
+	uint8_t targets;
 } dfuse_prefix;
 
 typedef struct {
-	int8_t device_low;
-	int8_t device_high;
-	int8_t product_low;
-	int8_t product_high;
-	int8_t vendor_low;
-	int8_t vendor_high;
-	int8_t dfu_low;
-	int8_t dfu_high;
+	uint8_t device_low;
+	uint8_t device_high;
+	uint8_t product_low;
+	uint8_t product_high;
+	uint8_t vendor_low;
+	uint8_t vendor_high;
+	uint8_t dfu_low;
+	uint8_t dfu_high;
 	char dfu_signature[3];
-	int8_t suffix_length;
-	int32_t crc;
+	uint8_t suffix_length;
+	uint32_t crc;
 } dfuse_suffix;
 
 typedef struct {
 	char signature[6];
-	int8_t alternate_setting;
+	uint8_t alternate_setting;
 	int32_t target_named;
 	char target_name[255];
-	int32_t target_size;
-	int32_t num_elements;
+	uint32_t target_size;
+	uint32_t num_elements;
 } dfuse_target_prefix;
 
 typedef struct {
-	int32_t element_address;
-	int32_t element_size;
-	int32_t * data;
+	uint32_t element_address;
+	uint32_t element_size;
+	uint8_t * data;
 } dfuse_image_element;
 
 typedef struct {
